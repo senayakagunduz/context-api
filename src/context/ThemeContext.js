de4d1ba453903d61e067a48e11acdda8badabfb5
senayakagunduz <senayakgndz@gmail.com>
2023-01-03 16:14:21 +0300

    added to local storage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,12 +1,18 @@
 import React,{createContext,useState} from 'react'
+import { useEffect } from 'react';
 
 const ThemeContext = createContext();
 
 export const ThemeProvider=({children})=>{
-    const [theme, setTheme]=useState('dark');
+    //local storageda ki varsayılan değeri useState e ekledik
+    //artık sayfayı yenilesek de değiştirdiğimiz mode aynı kalacak.
+    const [theme, setTheme]=useState(localStorage.getItem("theme") || "light");
     const values={
         theme,setTheme
     }
+    useEffect(()=>{
+        localStorage.setItem('theme',theme);
+    },[theme])
     return <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>
 }
 
